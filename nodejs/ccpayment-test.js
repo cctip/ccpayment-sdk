8d06b4c0aa73cceeccff2172e667b0aad0e2f11d
sunnibfgi <ja1523@proton.me>
2023-03-07 18:46:24 +0800

    update

---
+++ nodejs/ccpayment-test.js
@@ -0,0 +1,52 @@
+
+/* 
+ * This test code can only be displayed as a call method. 
+ * Related Nodejs Web service support can be run to run
+*/
+const appId = '202302160812171626132344467566592';
+const appSecret = 'a58f572564f7fce44acd66024d6da9b4';
+
+// init get appid and appsecret
+ccpaymentWidgets.init(appId, appSecret)
+// select checkout url
+ccpaymentWidgets.checkoutURL({
+  'valid_timestamp': 823456,
+  'amount': '1',
+  'merchant_order_id': '012033040550',
+  'product_name': 'test',
+  'return_url': 'https://app.gitbook.com/xxxxx'
+}, (result) => {
+  console.log('aaa:', result)
+})
+// select token
+ccpaymentWidgets.selectToken((result) => {
+  console.log('bbb:', result)
+})
+// select chain
+ccpaymentWidgets.selectChain({
+  "token_id": "8addd19b-37df-4faf-bd74-e61e214b008a"
+}, (result) => {
+  console.log('ccc:', result)
+})
+// submit order
+ccpaymentWidgets.submitOrder({
+  "remark": "eee",
+  "token_id": "8e5741cf-6e51-4892-9d04-3d40e1dd0128",
+  "chain": "TRX",
+  "amount": "0.5",
+  "contract": "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+  "merchant_order_id": "37350779790509509",
+  "fiat_currency": "USD"
+}, (result) => {
+  console.log('ddd:', result)
+})
+// token rate
+ccpaymentWidgets.tokenRate({
+  "amount": "1000",
+  "token_id": "f36ad1cf-222a-4933-9ad0-86df8069f916",
+}, (result) => {
+  console.log('eee:', result)
+})
+
+
+
