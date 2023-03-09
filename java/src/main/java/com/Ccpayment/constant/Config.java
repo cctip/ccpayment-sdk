26e4236ff876db0adce4b609224bddae68a215d2
david <nenu_1@protomail.cn>
2023-03-09 19:00:38 +0800

    #change variables

--- java/src/main/java/com/Ccpayment/constant/Config.java
+++ java/src/main/java/com/Ccpayment/constant/Config.java
@@ -1,15 +1,16 @@
 package com.Ccpayment.constant;
 
-//appId := "202302010636261620672405236006912"
-//appSecret := "62fbff1f796c42c50bb44d4d3d065390"
 // dev http://74ab25e1merchant.cwallet.com/ccpayment/v1
 // produce https://admin.ccpayment.com/ccpayment/v1
 public class Config {
     public static final String ApiUrl = "https://admin.ccpayment.com/ccpayment/v1";
-    public static final String AppId = "202302010636261620672405236006912";
-    public static final String AppSecrete = "62fbff1f796c42c50bb44d4d3d065390";
     public static final String HeaderAppId = "Appid";
     public static final String HeaderTimestamp = "Timestamp";
     public static final String HeaderSign = "Sign";
     public static final long expireDuration = 300L;
+
+    //modify
+    public static final String AppId = "202302010636261620672405236006912";
+    //modify
+    public static final String AppSecrete = "62fbff1f796c42c50bb44d4d3d065390";
 }
